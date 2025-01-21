0ced30f8036400f427754f4d3a5f3f68d2c486e9
avadhut-varvatkar <varvatkaraayush@gmail.com>
2025-01-21 23:44:27 +0530

    Array methods

---
+++ Array_Method.js
@@ -0,0 +1,42 @@
+let num = [1, 2, 3, 5, 55, 64, 58];
+console.log(num);
+
+/*
+let b = num.toString();
+console.log(b);
+
+let c = num.join("_");
+let d = num.join(" and ");
+console.log(c, d); 
+*/
+
+/*
+//num.pop() // removes last element from array
+ // console.log(num)
+let r1 = num.pop() // returns the popped element
+console.log(r1)
+*/
+
+/*
+//num.push(89) // adds new element at the end of array
+//console.log(num)
+let r2 = num.push(89);
+console.log(num, r2)
+*/
+
+/*
+//num.shift() // removes first element from array
+//console.log(num)
+let r3 = num.shift() // returns the shifted element
+console.log(r3)
+*/
+
+//num.unshift(44); // adds new element at the beginning of array
+//console.log(num);
+
+
+
+// Array from
+document.getElementsByClassName("class-name")
+let  a = document.getElementsByClassName("class-name")
+Array.from(a)
