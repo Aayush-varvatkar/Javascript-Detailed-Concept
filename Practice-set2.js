9120dffab6796c4d5c56f3206aeb95c4672a3ba8
avadhut-varvatkar <varvatkaraayush@gmail.com>
2025-01-25 23:49:17 +0530

    Practice question set2 by CWH

---
+++ Practice-set2.js
@@ -0,0 +1,48 @@
+// Question 1
+/*let age = prompt('Enter your age');
+
+if (age > 10 && age < 20) {
+  console.log('your age lies between 10 and 20');
+}
+else {
+  console.log('invalid')
+}*/
+
+//Question 2
+/*let a = 3;
+switch (a) {
+  case 1:
+    console.log('One');
+    break;
+  case 2:
+    console.log('two');
+    break;
+  case 3:
+    console.log('three');
+    break;
+  default:
+    console.log('Invalid')
+}*/
+
+//Question 3
+/*let num = prompt('Enter your number');
+//num = Number.parseInt(num);       --optional--
+if(num%2==0 && num%3==0){
+  console.log('number is divisible by 2 and 3');
+}
+else{
+  console.log('not divisible')
+}*/
+
+//Question 4
+/*let num2 = prompt('Enter');
+if(num2%2==0 || num2%3==0){
+  console.log('number is either divisible by 2 or 3');
+}
+else{
+  console.log('not divisible');
+}*/
+
+// Question 4
+/*let age = prompt('enter your age');
+console.log("You can", (age>18? "Drive": "not drive"));*/
