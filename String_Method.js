1ac6ae35efd5609fc722aa5c54ba9d1a3cd1a7dd
avadhut-varvatkar <varvatkaraayush@gmail.com>
2025-01-27 23:58:02 +0530

    Mostly imp String methods in javascript

---
+++ String_Method.js
@@ -0,0 +1,15 @@
+let str = "           Hey   there!         ";
+console.log(str.trim());      // removes the white spaces from both ends of the string
+
+let str1 = "Hello there";
+console.log(str1.slice(3,7))   // returns the substring from index 3 to 7
+
+let str2 = "Hello I'm Aayush";
+let str3 = "I love javascript";
+console.log(str2.concat(str3));    // concatenates the two strings
+
+let str4 = "Baka";
+console.log(str4.replace("a", "o"));    // replaces the first occurence of a with o
+
+let str5 = "Hello there";
+console.log(str5.charAt(7));    // returns the character at index 7
