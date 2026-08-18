d32533fbff5e6972d1b38125271d95d2e066c6fb
Trevino20 <Varvatkaraayush@gmail.com>
2026-08-18 17:53:57 +0530

    resolved readme.md file conflict.

--- Array_Method.js
+++ Array_Method.js
@@ -1,7 +1,12 @@
-let num = [1, 2, 3, 5, 55, 64, 58];
+let num = [1,2,3,5,55,64,58];
 console.log(num);
 
-/*
+
+
+
+
+/* tostring() and join() methods
+
 let b = num.toString();
 console.log(b);
 
@@ -10,33 +15,86 @@ let d = num.join(" and ");
 console.log(c, d); 
 */
 
+
+
+
+
+//pop() method        // removes last element from array
+
 /*
-//num.pop() // removes last element from array
- // console.log(num)
+console.log(num)
 let r1 = num.pop() // returns the popped element
 console.log(r1)
 */
 
+
+
+
+
+//push() method      // adds new element at the end of array
+
 /*
-//num.push(89) // adds new element at the end of array
-//console.log(num)
+console.log(num)
 let r2 = num.push(89);
 console.log(num, r2)
 */
 
+
+
+
+
+//shift() method     // removes first element from array
+
 /*
-//num.shift() // removes first element from array
-//console.log(num)
+console.log(num)
 let r3 = num.shift() // returns the shifted element
 console.log(r3)
 */
 
-//num.unshift(44); // adds new element at the beginning of array
-//console.log(num);
 
 
 
-// Array from
-document.getElementsByClassName("class-name")
-let  a = document.getElementsByClassName("class-name")
-Array.from(a)
+
+
+// unshift(); method        // adds new element at the beginning of array
+
+/*
+let add = num.unshift(44); // returns the new length of the array
+console.log(num);
+*/
+
+
+
+
+
+// concat() method           // joins two or more arrays
+
+/*
+let hero1 = ["Spiderman", "Batman", "Superman", "Antman"];
+let hero2 = ["vulture", "albedo", "gobline", "Thanos"];
+
+let newHero = hero1.concat(hero2);
+console.log(newHero);
+*/
+
+
+
+
+
+//slice() method           // extracts a section of an array and returns it as a new array
+//slice(strinx, endidx);
+
+/*
+let hero1 = ["Spiderman", "Batman", "Superman", "Antman", "vulture", "albedo", "gobline", "Thanos"];
+console.log(hero1.slice(2,4));
+*/
+
+
+
+
+
+
+//  Array from
+// document.getElementsByClassName("class-name")
+// let  a = document.getElementsByClassName("class-name")
+// Array.from(a)
--- Concept.md
+++ Concept.md
@@ -1,3 +1,18 @@
 > Var :  var can be redeclared and updated, it's a global scope variable
   let :  variable cannot be re-declared but can be updated.  block scope variable
   const:  const can't be redeclared and updated since it's constant 
+
+
+> Strings are immutable
+  Arrays are mutable
+
+
+  
+> DOM_INSERT_ELEMENT
+
+let el = document.createElement("button");          // creates a new button element
+
+node.append(el);                                  // adds new-element at the end of the node (INSIDE)
+node.prepend(el);                                 // adds new-element at the start of the node (INSIDE)
+node.before(el);                                  // adds new-element before the node (OUTSIDE)
+node.after(el);                                   // adds new-element after the node (OUTSIDE)
